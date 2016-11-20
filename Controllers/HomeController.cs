47b21eb61c9691b755c5455906599668f007d707
akaramanapp <abdulkerimkaraman@gmail.com>
2016-11-20 11:45:22 +0200

    FirstProjecIos modülünün ilk iadesi

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace FirstProjecIos.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var customer = GetCustomer();
+			return View(customer);
+		}
+
+		public List<Customer> GetCustomer()
+		{
+
+			var customers = new List<Customer>();
+			customers.Add(new Customer { Id = 1, Name = "Microsft", Address = "Redmond, Washington, ABD" });
+			customers.Add(new Customer { Id = 2, Name = "Apple", Address = "Cupertino, Kaliforniya, ABD" });
+			return customers;
+		}
+	}
+
+}
